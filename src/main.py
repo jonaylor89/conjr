842a8e2d3fd8dbcdc1bdc086e91b6f91c5982c0c
John Naylor <jonaylor89@gmail.com>
2019-06-03 12:34:22 -0400

    Test google docs

--- src/main.py
+++ src/main.py
@@ -1,9 +1,72 @@
 #!/usr/bin/env python3
 
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/documents']
+
+# The ID of a sample document.
+DOCUMENT_ID = '19COFj7k2f1jxn0g87BxmXFOJHZlCociTVMAMxkxDKuk'
+
 def main():
-    pass
-          
+    """
+    Shows basic usage of the Docs API.
+    Prints the title of a sample document.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server()
+
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('docs', 'v1', credentials=creds)
+    requests = [
+         {
+            'insertText': {
+                'location': {
+                    'index': 1,
+                },
+                'text': '\n'
+            }
+        },
+        {
+            'insertText': {
+                'location': {
+                    'index': 2,
+                },
+                'text': 'Hello World'
+            }
+        },
+    ]
+
+    result = service.documents().batchUpdate(
+        documentId=DOCUMENT_ID, body={'requests': requests}).execute()
+
+    # Retrieve the documents contents from the Docs service.
+    document = service.documents().get(documentId=DOCUMENT_ID).execute()
+
+    print('The title of the document is: {}'.format(document.get('title')))
+
+
 if __name__ == '__main__':
     main()
-      
-        
+
