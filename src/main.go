f73d56e6d864640a9c7338f8ca22c6568c14ba48
John Naylor <jonaylor89@gmail.com>
2019-08-08 18:51:41 -0400

    python -> golang

---
+++ src/main.go
@@ -0,0 +1,244 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strconv"
+
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/sheets/v4"
+)
+
+// Config : Configuration file structure
+type Config struct {
+	env          string `json:"env"`
+	speadsheetID string `json:"speadsheet_id"`
+	scopes       string `json:"scopes"`
+	sheetRange   string `json:"range"`
+}
+
+// Retrieve a token, saves the token, then returns the generated client.
+func getClient(config *oauth2.Config) *http.Client {
+	// The file token.json stores the user's access and refresh tokens, and is
+	// created automatically when the authorization flow completes for the first
+	// time.
+	tokFile := "token.json"
+	tok, err := tokenFromFile(tokFile)
+	if err != nil {
+		tok = getTokenFromWeb(config)
+		saveToken(tokFile, tok)
+	}
+	return config.Client(context.Background(), tok)
+}
+
+// Request a token from the web, then returns the retrieved token.
+func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser then type the "+
+		"authorization code: \n%v\n", authURL)
+
+	var authCode string
+	if _, err := fmt.Scan(&authCode); err != nil {
+		log.Fatalf("Unable to read authorization code: %v", err)
+	}
+
+	tok, err := config.Exchange(context.TODO(), authCode)
+	if err != nil {
+		log.Fatalf("Unable to retrieve token from web: %v", err)
+	}
+	return tok
+}
+
+// Retrieves a token from a local file.
+func tokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	tok := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(tok)
+	return tok, err
+}
+
+// Saves a token to a file path.
+func saveToken(path string, token *oauth2.Token) {
+	fmt.Printf("Saving credential file to: %s\n", path)
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		log.Fatalf("Unable to cache oauth token: %v", err)
+	}
+	defer f.Close()
+	json.NewEncoder(f).Encode(token)
+}
+
+func getKalturaConfig(path string) map[string]interface{} {
+
+	var kaltura map[string]interface{}
+
+	// Open our jsonFile
+	jsonFile, err := os.Open("config.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Successfully Opened config.json")
+
+	// defer the closing of our jsonFile so that we can parse it later on
+	defer jsonFile.Close()
+
+	// read our opened xmlFile as a byte array.
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	// we unmarshal our byteArray which contains our
+	// jsonFile's content into 'config' which we defined above
+	json.Unmarshal(byteValue, &kaltura)
+
+	return kaltura
+}
+
+func updateKalturaSettings(path []byte, newSettings map[string]interface{}) {
+
+	marshalledSettings, _ := json.Marshal(newSettings)
+	err := ioutil.WriteFile(string(path), marshalledSettings, 0644)
+
+	if err != nil {
+		fmt.Println("[ERROR] new kaltura config couldn't be written to")
+	}
+}
+
+func getConfig() *Config {
+
+	var config Config
+
+	// Open our jsonFile
+	jsonFile, err := os.Open("config.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Successfully Opened config.json")
+
+	// defer the closing of our jsonFile so that we can parse it later on
+	defer jsonFile.Close()
+
+	// read our opened xmlFile as a byte array.
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	// we unmarshal our byteArray which contains our
+	// jsonFile's content into 'config' which we defined above
+	json.Unmarshal(byteValue, &config)
+
+	return &config
+}
+
+func main() {
+
+	var serialNumber []byte
+	var localSettingsPath []byte
+
+	config := getConfig()
+
+	if config.env == "dev" {
+		serialNumber = []byte("3WFBH2")
+		localSettingsPath = []byte("localSettings.json")
+	} else if config.env == "prod" {
+		// Find the Kaltura local settings
+		houstinsConfigPath := filepath.Join(os.Getenv("SystemDrive"), "\\VCU-Deploy\\config\\Kaltura\\config.ps1")
+
+		var err error
+		localSettingsPath, err = exec.Command("powershell.exe", houstinsConfigPath).Output()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		serialNumber, err = exec.Command("powershell.exe", "gwmi win32_bios serialnumber | Select -ExpandProperty serialnumber").Output()
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		log.Fatal("[ERROR] unknown 'env' in configuration file (must be 'dev' or 'prod') or environment variables not set properly")
+	}
+
+	if _, err := os.Stat(string(localSettingsPath)); err != nil {
+		log.Fatal("[ERROR] unable to find kaltura local settings (localSettings.json)")
+	}
+
+	// Grab kaltura settings
+	kaltura := getKalturaConfig(string(localSettingsPath))
+	resourceID := ((kaltura["config"].(map[string]interface{}))["shared"].(map[string]interface{}))["resrouceId"].(string)
+
+	if _, err := os.Stat("credentials.json"); err != nil {
+		log.Fatal("[ERROR] missing Google API credentials (credentials.json)")
+	}
+
+	b, err := ioutil.ReadFile("credentials.json")
+	if err != nil {
+		log.Fatalf("Unable to read client secret file: %v", err)
+	}
+
+	// If modifying these scopes, delete your previously saved token.json.
+	gConfig, err := google.ConfigFromJSON(b, config.scopes)
+	if err != nil {
+		log.Fatalf("Unable to parse client secret file to config: %v", err)
+	}
+	client := getClient(gConfig)
+
+	srv, err := sheets.New(client)
+	if err != nil {
+		log.Fatalf("Unable to retrieve Sheets client: %v", err)
+	}
+
+	resp, err := srv.Spreadsheets.Values.Get(config.speadsheetID, config.sheetRange).Do()
+	if err != nil {
+		log.Fatalf("Unable to retrieve data from sheet: %v", err)
+	}
+
+	// Make sure there is data
+	if len(resp.Values) == 0 {
+		fmt.Println("[ERROR] No data found.")
+	} else {
+		fmt.Println("Name, Major:")
+		for _, row := range resp.Values {
+			if row[0].(string) == string(serialNumber) {
+				temp, _ := strconv.Atoi(row[19].(string))
+				if row[19] != resourceID && temp == 0 {
+					row[19] = resourceID
+
+					var vr sheets.ValueRange
+					vr.Values = resp.Values
+
+					_, err := srv.Spreadsheets.Values.Update(
+						config.speadsheetID,
+						config.sheetRange,
+						&vr,
+					).ValueInputOption("USER_ENTERED").Do()
+
+					if err != nil {
+						log.Fatalf("unable to update sheet %v", err)
+					}
+
+					fmt.Println("[INFO] cells updated")
+					return
+				} else if row[19] != resourceID {
+					fmt.Println("[INFO] changing local settings to reflect spreadsheet")
+
+					// TODO: Update kaltura json
+					updateKalturaSettings(localSettingsPath, kaltura)
+
+				} else {
+					fmt.Println("[INFO] nothing to change for " + row[0].(string))
+					return
+				}
+			}
+		}
+	}
+}
