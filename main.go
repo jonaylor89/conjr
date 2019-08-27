51ef2856484fa61e7881a91480180dff54eeab05
John Naylor <jonaylor89@gmail.com>
2019-08-27 13:50:45 -0400

    Download binary

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"http"
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
@@ -116,9 +118,35 @@ func getConfig() (*Config, error) {
 	return &config, nil
 }
 
+// DownloadFile will download a url to a local file
+func DownloadFile(filepath string, url string) error {
+
+    // Get the data
+    resp, err := http.Get(url)
+    if err != nil {
+        return err
+    }
+    defer resp.Body.Close()
+
+    // Create the file
+    out, err := os.Create(filepath)
+    if err != nil {
+        return err
+    }
+    defer out.Close()
+
+    // Write the body to file
+    _, err = io.Copy(out, resp.Body)
+    return err
+}
+
 func installMSI(binParams *BinaryParameters, installParams *InstallParameters) error {
 
-	// TODO: Download Binary
+	// Download Binary
+	err := downloadFile(binParam.FileLocation, binParam.URL)
+	if err != nil {
+		return err
+	}
 
 	tmplString := `/i %s /qn /norestart
 		INSTALLDIR=%s
@@ -303,8 +331,8 @@ func main() {
 			log.Fatal(err)
 		}
 
-		fmt.Printf("Serial Number (%s) added to the googlesheet\n", serialNumber)
-		fmt.Println(resp)
+		log.Printf("[INFO] serial Number (%s) added to the googlesheet\n", serialNumber)
+		log.Println(resp)
 
 	}
 }
